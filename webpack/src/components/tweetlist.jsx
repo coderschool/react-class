4d50304640542af892c09bed166d14e0e12981fa
Jieren Chen <jieren.chen@gmail.com>
2017-10-28 12:15:59 +0700

    fix loading

--- webpack/src/components/tweetlist.jsx
+++ webpack/src/components/tweetlist.jsx
@@ -1,9 +1,7 @@
 import React, { PropTypes } from 'react';
-import _ from 'lodash';
 import Immutable from 'immutable';
 import TweetItem from './tweetitem';
 import {Grid, Row} from 'react-bootstrap';
-import Loading from 'react-loading-spinner';
 
 export default class TweetListComponent extends React.Component {
 
@@ -14,29 +12,32 @@ export default class TweetListComponent extends React.Component {
   render () {
     const isLoading = this.props.remote.getIn(['tweets', 'pending']) || false;
 
-    const tweets = this.props.remote.getIn(['tweets', 'items']) || [];
-
-    const items = tweets.map((tweet) => {
-      const tweetJs = tweet.toJS()
-      const likeTweet = () => {
-        return this.props.likeTweet(tweetJs.id);
-      }
+    if (isLoading) {
       return (
-        <Row key={tweetJs.id}>
-          <TweetItem id={tweetJs.id}
-                     text={tweetJs.text} 
-                     likes={tweetJs.likes}
-                     likeTweet={likeTweet} />
-        </Row>
-      );
-    })
+        <h1>Loading...</h1>
+      )
+    } else {
+      const tweets = this.props.remote.getIn(['tweets', 'items']) || [];
+      const items = tweets.map((tweet) => {
+        const tweetJs = tweet.toJS()
+        const likeTweet = () => {
+          return this.props.likeTweet(tweetJs.id);
+        }
+        return (
+          <Row key={tweetJs.id}>
+            <TweetItem id={tweetJs.id}
+                       text={tweetJs.text} 
+                       likes={tweetJs.likes}
+                       likeTweet={likeTweet} />
+          </Row>
+        );
+      });
 
-    return (
-      <Loading isLoading={isLoading}>
+      return (
         <Grid>
           {items}
         </Grid>
-      </Loading>
-    );
+      );
+    }
   }
 }
